a74bd6c5bdea54846d576d02cef1caaaf8a1ea40
Paul Givel <paul.givel@damaemedical.fr>
2020-06-16 18:32:47 +0200

    plug true data for next launch, from API fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NextLaunch from "./NextLaunch";
 import Wrapper from "./Wrapper";
 
 function App() {
+  const [nextLaunch, setNextLaunch] = useState({
+    mission_name: "",
+    launch_date_utc: new Date(),
+    links: { mission_patch_small: "" },
+    details: ""
+  });
+
+  async function fetchNextLaunch() {
+    let result = await fetch("https://api.spacexdata.com/v3/launches/next");
+    let data = await result.json();
+    return data;
+  }
+
+  useEffect(() => {
+    fetchNextLaunch().then((data) => setNextLaunch(data));
+  }, []);
+
+  let dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour12: true,
+  };
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -12,8 +36,10 @@ function App() {
       </header> */}
       {/* <Player></Player> */}
       <div className="Content">
-        <Wrapper title="Recent activity">{/* <LastLaunches></LastLaunches> */}</Wrapper>
-        <NextLaunch></NextLaunch>
+        <Wrapper title="Recent activity">
+          {/* <LastLaunches></LastLaunches> */}
+        </Wrapper>
+        <NextLaunch launch={nextLaunch}></NextLaunch>
       </div>
     </div>
   );
